176455c2ba1f88192cd7f7d7eaa75cac06f49b3c
ymh0000123 <ymh0000123@163.com>
2024-08-14 15:35:32 +0800

    优化体验

--- src/components/menuleft.vue
+++ src/components/menuleft.vue
@@ -6,6 +6,11 @@
             <h1 id="name">{{ appName }}</h1>
         </div>
     </div>
+    <div id="button">
+        <div id="button-1">
+            <button @click="open('/')">Home</button>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -15,11 +20,27 @@ export default {
         return {
             appName: import.meta.env.VITE_NAME  // 从环境变量中获取应用名称
         }
+    },
+    setup() {
+        const open = (url) => {
+            // 检查url是否以 / 开头
+            if (url.startsWith('/')) {
+                // 如果以 / 开头，在当前页面导航
+                window.location.href = url;
+            } else {
+                // 如果不是以 / 开头，在新标签页中打开
+                window.open(url, "_blank");
+            }
+        }
+
+        return {
+            open
+        }
     }
 }
 </script>
 <style scoped>
 #name {
-    color: rgb(192,228,233);
+    color: rgb(192, 228, 233);
 }
 </style>
